57f23131088d166be8cc94df2e70c8bab940f016
Abhishek Rawat <abhishektgt5@gmail.com>
2025-08-04 00:39:47 +0530

    changed the login api call address to direct adress to check if login works and it will proof that it is a probelm of that env variable

--- src/login.js
+++ src/login.js
@@ -15,7 +15,7 @@ const Login = ({ onLogin }) => {
     setMessage("");
 
     try {
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
+      const response = await axios.post("https://chatmate-backend-z54c.onrender.com/api/auth/login", {
         email,
         password,
       });
